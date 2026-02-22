5d8c3162f63dc8a67a63af312eb1a311fb9fe84e
CongMinh2211 <congminh2211@gmail.com>
2026-02-22 20:45:20 +0700

    Update: Sync bank account info to MSB 7008032005

--- app/Http/Controllers/SepayController.php
+++ app/Http/Controllers/SepayController.php
@@ -49,8 +49,8 @@ class SepayController
 
         // Thông tin ngân hàng MSB (Maritime Bank)
         $bankName = 'MSB';
-        $bankAccount = '968866976549';
-        $bankAccountName = 'NGUYENTHITUYETNHU DKMN';
+        $bankAccount = '7008032005';
+        $bankAccountName = 'TRAN CONG MINH';
         $bankBin = '970426';
 
         // Tạo URL QR VietQR
@@ -336,8 +336,8 @@ class SepayController
         }
 
         $bankName = 'MSB';
-        $bankAccount = '968866976549';
-        $bankAccountName = 'NGUYENTHITUYETNHU DKMN';
+        $bankAccount = '7008032005';
+        $bankAccountName = 'TRAN CONG MINH';
         $bankBin = '970426';
         $soTien = $datTour->tien_thuc_nhan;
 
